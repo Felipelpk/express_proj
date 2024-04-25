385b6e93631a6bc62a92a9c1a103447e2459d441
Felipe Alves <felipealves@Felipes-MacBook-Air-2.local>
2024-04-25 12:20:56 -0300

    new route

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,8 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get("/download", (req, res) => res.send("Download Route"));
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
