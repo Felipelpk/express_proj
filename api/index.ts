f0ef1cd43a5dd51e30085ba1806c72e4ec9da80f
Felipe Alves <felipealves@Felipes-MacBook-Air-2.local>
2024-05-03 02:10:42 -0300

    [Fix] Adding Temp Dir

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -6,19 +7,37 @@ import path from "path";
 import routes from './routes/index';
 
 import os from "os";
-const tempDir = os.tmpdir();
+export const tempDir = os.tmpdir();
 
 const app = express();
-const templatesDir = path.join(tempDir, "views");
+export const temporaryTemplatesDir = path.join(tempDir, "views");
+const templatesDir = path.join(__dirname, 'views');
 
-console.log("Templates Dir =>", templatesDir);
+function copyFilesToTempDir(sourceDir: any, tempDir: any) {
+    // Certifique-se de que o diretório temporário existe
+    if (!fs.existsSync(tempDir)) {
+      fs.mkdirSync(tempDir, { recursive: true });
+    }
+  
+    // Ler arquivos do diretório de origem
+    const files = fs.readdirSync(sourceDir);
+  
+    // Copiar cada arquivo para o diretório temporário
+    files.forEach(file => {
+      const sourceFile = path.join(sourceDir, file);
+      const destFile = path.join(tempDir, file);
+      fs.copyFileSync(sourceFile, destFile);
+    });
+};
+
+copyFilesToTempDir(templatesDir, temporaryTemplatesDir);
 
 app.use(cors());
 app.use(express.text());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-nunjucks.configure(templatesDir, {
+nunjucks.configure(temporaryTemplatesDir, {
     autoescape: true,
     express: app,
     watch: true,
--- api/routes/post-generate-html.route.ts
+++ api/routes/post-generate-html.route.ts
@@ -3,17 +3,18 @@ import path from "path";
 import fs from "fs";
 import { footer } from "../footer";
 import nunjucks from "nunjucks";
+import { tempDir } from "../index";
 
 interface RequestBody {
   components: string[];
   background?: string;
-}
+};
 
 export const postGenerateHTML = (req: express.Request<{}, {}, RequestBody>, res: express.Response) => {
   const { components, background } = req.body;
     const bgColor = background ?? "#F6F6F6";
 
-    const pathTemplate = path.join("./api", "views", "template.njk");
+    const pathTemplate = path.join(tempDir, "views", "template.njk");
     const initialHTML = `<!doctype html>\n<html>\n<head>\n<meta charset="utf-8">\n<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">\n<title>Document</title>\n<style>\nbody{margin:0; padding:0}\nimg{margin:0; padding:0; }\na[href^=tel]{ color:#666666; text-decoration:none;}\na[href^=date]{ color:#666666; text-decoration:none;}\nhr {margin:0 !important}\ndiv, p, a, li, td {-webkit-text-size-adjust:none;}\n.inlineblock>tbody,\n.inlineblock>tbody>tr,\n.inlineblock>tbody>tr>td {display: block; width: 100%}\n@media only screen and (max-width: 600px) {\nimgmobile{max-width: 70%}\n}\n</style>\n</head>\n<body>\n<div style="margin:0; padding:0;" bgcolor="#ffffff">\n<table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" style="table-layout: fixed;" bgcolor="#F6F6F6"><tr><td align="center" valign="top">\n<!--[if mso]><table width="650" cellpadding="0" cellspacing="0" border="0" align="center"><tr><td align="center" valign="top"><![endif]-->\n<table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" style="max-width:650px" bgcolor="${bgColor}">`;
 
     try {
@@ -32,7 +33,7 @@ export const postGenerateHTML = (req: express.Request<{}, {}, RequestBody>, res:
         fs.appendFileSync(pathTemplate, footer);
 
         const generateHTML = nunjucks.render("template.njk");
-        const pathOutputHTML = path.join("./api", "output.html");
+        const pathOutputHTML = path.join(tempDir, "output.html");
 
         fs.writeFileSync(pathOutputHTML, generateHTML);
 
