2702a13498391cf3bc8781110abaa4d13ce56c46
Felipe Alves <felipealves@Felipes-MacBook-Air-2.local>
2024-05-03 01:59:10 -0300

    [Fix] BGColor Error

--- api/header.ts
+++ api/header.ts
@@ -24,6 +24,6 @@ div, p, a, li, td {-webkit-text-size-adjust:none;}
 <div style="margin:0; padding:0;" bgcolor="#ffffff">
 <table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" style="table-layout: fixed;" bgcolor="#F6F6F6"><tr><td align="center" valign="top">
 <!--[if mso]><table width="650" cellpadding="0" cellspacing="0" border="0" align="center"><tr><td align="center" valign="top"><![endif]-->
-<table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" style="max-width:650px" bgcolor="#${bgcolor}">`
+<table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" style="max-width:650px" bgcolor="#000">`
 
 exports.HTMLbody1 = header;
